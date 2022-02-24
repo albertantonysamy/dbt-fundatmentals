fd51bcbf07607a6dbb634d09f84acaa7acb7b05f
albert.antonysamy@windriver.com <albert.antonysamy@windriver.com>
2022-02-24 14:25:42 +0000

    fct_orders

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,13 @@
+with payments as (
+
+    select 
+        id as payment_id,
+        orderid as order_id,
+        amount
+
+        from raw.stripe.payment
+            where status = 'success'
+
+)
+
+select * from payments
