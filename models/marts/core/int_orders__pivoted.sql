cc4ae61416f0b1aac27f726040c7679fbe7c3251
albert.antonysamy@windriver.com <albert.antonysamy@windriver.com>
2022-03-07 19:51:12 +0000

    jinja pivoted

---
+++ models/marts/core/int_orders__pivoted.sql
@@ -0,0 +1,26 @@
+{%- set payment_methods = ['bank_transfer', 'credit_card', 'coupon', 'gift_card'] -%}
+
+with payments as (
+   select * from {{ ref('stg_payments') }}
+),
+
+final as (
+   select
+       order_id,
+       {% for paymentmethod in payment_methods -%}
+
+       sum(case when paymentmethod = '{{ payment_method }}' then amount else 0 end)
+            as {{ paymentmethod }}_amount
+
+       {%- if not loop.last -%}
+         ,
+       {% endif -%}
+
+       {%- endfor %}
+   from {{ ref('stg_payments') }}
+   group by 1
+)
+
+select * from final
+
+
